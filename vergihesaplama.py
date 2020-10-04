f4bc4a80ebac8c28f01ff5381148e0ceeae075f6
Ayaz Armutlu <ayazayazilim@gmail.com>
2020-10-04 12:03:17 +0300

    Create vergihesaplama.py

---
+++ vergihesaplama.py
@@ -0,0 +1,10 @@
+#Bu Çalışma NY da asgeri Ücretli Birinin maksimum vergi oranı 
+#düşüldüğünde alabiliceğil en düşük maaştır.
+
+
+gunluk_maas = 15*9
+haftalık_maas = gunluk_maas * 5
+aylık_maas = haftalık_maas * 4
+vergi = aylık_maas * 0.3
+Brut_aylık_maas = aylık_maas - vergi
+print(Brut_aylık_maas)
